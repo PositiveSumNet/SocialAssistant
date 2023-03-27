6ead6a5d048e6e84e53871026f1b2d33f7fce139
Vince Scafaria <vince@positivesum.net>
2023-03-27 13:54:02 -0400

    content and background js are now clean

--- background.js
+++ background.js
@@ -16,18 +16,18 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   return true;
 });
 
-const processSave = async function(follows) {
-  await injectImageBase64s(follows);
-  saveToTempStorage(follows);
-  return {saved: follows, success: true};
+const processSave = async function(records) {
+  await injectImageBase64s(records);
+  saveToTempStorage(records);
+  return {saved: records, success: true};
 }
 
 // TODO: Parallel tasks like one of these
 // bytelimes.com/batch-async-tasks-with-async-generators/
 // stackoverflow.com/questions/35612428/call-async-await-functions-in-parallel
-const injectImageBase64s = async function(follows) {
-  for (let i = 0; i < follows.length; i++) {
-    let item = follows[i];
+const injectImageBase64s = async function(records) {
+  for (let i = 0; i < records.length; i++) {
+    let item = records[i];
     if (item.imgCdnUrl) {
       item.img64Url = await getImageBase64(item.imgCdnUrl);
     }
@@ -35,10 +35,10 @@ const injectImageBase64s = async function(follows) {
 }
 
 // caches what we'll want to persist to the sqlitedb when we get the chance
-const saveToTempStorage = function(follows) {
+const saveToTempStorage = function(records) {
   // the 'fordb-' prefix is how we find all such pending batches
   const key = 'fordb-' + Date.now().toString();
-  chrome.storage.local.set({ [key]: follows });
+  chrome.storage.local.set({ [key]: records });
 }
 
 // stackoverflow.com/questions/57346889/how-to-return-base64-data-from-a-fetch-promise
--- content.js
+++ content.js
@@ -4,31 +4,38 @@
 // manifest shows other included files
 // also see lib/recordinglib.js (including for public variables)
 
+/*
+  APPLICATION FLOW:
+  
+  On Startup
+    startRecording() if cached setting suggests we should
+  
+  On click 'record' or stop recording from the popup
+    listenForRecordingToggle() starts/stops recording and auto-scroll
+
+  When we're recording, a MutationObserver looks for newly added nodes (plus when turned on, finds relevant ones that are already there)
+  and converts them to the savable records. Every 5 seconds, these are saved to localStorage (using background.js) thanks to RECORDING.setSaveTimer. 
+  When the DB UI is opened, records are flushed from localStorage to the SQLite database.
+  
+*/
+
 // on startup, see if supposed to already be recording
 chrome.storage.local.get(['recording'], function(result) {
   if (result.recording === true) {
     // here at startup, extension is in a 'load if we can' state
-    const parsedUrl = URLPARSE.getParsedUrl();
-    
-    if (parsedUrl) {
-      const recorder = RECORDERFACTORY.getRecorder(parsedUrl.pageType);
-      
-      if (recorder) {
-        recorder.startRecording();
-      }
+    const recorder = RECORDING.getRecorder();
+
+    if (recorder) {
+      recorder.startRecording();
     }
   }
 });
 
 // toggle recording and auto-scroll on/off
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  const parsedUrl = URLPARSE.getParsedUrl();
+  const recorder = RECORDING.getRecorder();
   
-  if (parsedUrl) {
-    const recorder = RECORDERFACTORY.getRecorder(parsedUrl.pageType);
-    
-    if (recorder) {
-      recorder.listenForRecordingToggle(request, sender, sendResponse);
-    }
+  if (recorder) {
+    recorder.listenForRecordingToggle(request, sender, sendResponse);
   }
 });
