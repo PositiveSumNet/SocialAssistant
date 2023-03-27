26358dd236b69d46361b8c96e76c355ad2b9cbd7
Vince Scafaria <vince@positivesum.net>
2023-03-27 12:34:23 -0400

    refactoring content js

--- content.js
+++ content.js
@@ -2,20 +2,20 @@
 // cpomagazine.com/data-privacy/what-the-hiq-vs-linkedin-case-means-for-automated-web-scraping/
 
 // for recording
-var _savableFollows = [];
-var _savableFollowKeySet = new Set(); // to avoid storing dupes
-var _savedFollowHandleSet = new Set();
-var _twitterObserver;
+var _savables = [];
+var _savableKeySet = new Set(); // to avoid storing dupes
+var _savedKeySet = new Set();
+var _observer;
 // for scrolling
+var _autoScroll = false;
 var _lastDiscoveryTime = null;
 var _lastScrollTime = null;
 var _emptyScrollCount = 0;
 var _preScrollCount = 0;
-var _autoScroll = false;
 var _scrollIsPending = false;
 var _countWhenScrollDoneSet;
 
-var _mutationSettings = { 
+var _observerSettings = { 
   attributes: false, 
   childList: true, 
   subtree: true
@@ -47,11 +47,20 @@ const tryStartRecordingTwitter = function() {
   }
 }
 
+const shouldAvoidScrollIfHidden = function(site) {
+  switch (site) {
+    case SITE.TWITTER:
+      return true;
+    default:
+      return false;
+  }
+}
+
 const startRecording = function() {
   const parsedUrl = URLPARSE.getParsedUrl();
   const site = parsedUrl.site;
   
-  if (!_twitterObserver && site === SITE.TWITTER) {
+  if (!_observer && site === SITE.TWITTER) {
     // begin recording
     recordTwitter();
     // periodically check for collected items to save
@@ -59,39 +68,38 @@ const startRecording = function() {
   }
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    switch (request.actionType) {
-      case MSGTYPE.RECORDING.START:
-        startRecording();
-        if (request.auto === true && _autoScroll === false) {
-          _autoScroll = true;
-          const parsedUrl = URLPARSE.getParsedUrl();
-          const avoidScrollIfHidden = (parsedUrl.site === SITE.TWITTER);
-          scrollAsNeeded(avoidScrollIfHidden);
-        }
-        
-        break;
-      case MSGTYPE.RECORDING.STOP:
-        stopRecording('');
-        break;
-      default:
-        break;
-    }
-    
-    sendResponse({auto: request.auto, success: true});
+// toggle recording and auto-scroll on/off
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.actionType) {
+    case MSGTYPE.RECORDING.START:
+      startRecording();
+      if (request.auto === true && _autoScroll === false) {
+        _autoScroll = true;
+        const parsedUrl = URLPARSE.getParsedUrl();
+        const avoidScrollIfHidden = shouldAvoidScrollIfHidden(parsedUrl.site);
+        scrollAsNeeded(avoidScrollIfHidden);
+      }
+      
+      break;
+    case MSGTYPE.RECORDING.STOP:
+      stopRecording('');
+      break;
+    default:
+      break;
   }
-);
+  
+  sendResponse({auto: request.auto, success: true});
+});
 
 const reinit = function() {
-  _savableFollows = [];
-  _savableFollowKeySet = new Set(); // to avoid storing dupes
-  _savedFollowHandleSet = new Set();
+  _savables = [];
+  _savableKeySet = new Set(); // to avoid storing dupes
+  _savedKeySet = new Set();
 
-  if (_twitterObserver) {
-    _twitterObserver.disconnect();
+  if (_observer) {
+    _observer.disconnect();
   }
-  _twitterObserver = undefined;
+  _observer = undefined;
 
   _lastDiscoveryTime = null;
   _lastScrollTime = null;
@@ -109,28 +117,28 @@ const stopRecording = function(badgeText) {
 }
 
 const setFollowSaveTimer = function() {
-  if (!_twitterObserver) {
+  if (!_observer) {
     return;
   }
   
-  if (_savableFollows.length > 0) {
+  if (_savables.length > 0) {
     // tell background js to save to local storage cache
     chrome.runtime.sendMessage(
     {
       actionType: 'save',
-      payload: _savableFollows
+      payload: _savables
     }, 
     function(response) {
       if (response && response.success === true && response.saved && response.saved.length > 0) {
         const newSaveCnt = response.saved.length;
         for (let i = 0; i < response.saved.length; i++) {
           let item = response.saved[i];
-          if (!_savedFollowHandleSet.has(item.handle.toLowerCase())) {
-            _savedFollowHandleSet.add(item.handle.toLowerCase());
+          if (!_savedKeySet.has(item.handle.toLowerCase())) {
+            _savedKeySet.add(item.handle.toLowerCase());
           }
         }
         
-        if (_countWhenScrollDoneSet != _savableFollowKeySet.size + _savedFollowHandleSet.size) {
+        if (_countWhenScrollDoneSet != _savableKeySet.size + _savedKeySet.size) {
           // tell background js to update badge (the condition is so that we don't set to a number when 'DONE' was in place)
           
           const badgeText = '+' + newSaveCnt;
@@ -143,24 +151,14 @@ const setFollowSaveTimer = function() {
     });
     
     // clear
-    _savableFollows = [];
-    _savableFollowKeySet = new Set();
+    _savables = [];
+    _savableKeySet = new Set();
   }
   
   // every 5 seconds, see if time to save
   setTimeout(() => {
     setFollowSaveTimer();
   }, 5000);
-}
-
-const getTwitterFollowImgs = function(scopeElem) {
-  if (TPARSE.isTwitterProfilePhoto(scopeElem)) {
-    return [scopeElem];
-  }
-  else {
-    // all img elms with src that starts with the tell-tale prefix
-    return Array.from(scopeElem.querySelectorAll(`img[src*="${TPARSE.PROFILE_IMAGES_HINT}"]`));
-  }
 }
 
 const twitterFollowMutationCallback = function(mutations) {
@@ -170,10 +168,6 @@ const twitterFollowMutationCallback = function(mutations) {
       for (let i = 0; i < nodes.length; i++) {
         let node = nodes[i];
         
-        // The twitter profile photo points upward to anchor with href of '/myhandle' 
-        // then upward to div with data-testid of UserCell.
-        // The UserCell has two anchor elements (other than the img anchor), the first for DisplayName and the next with Handle (myhandle).
-        // So we can grab everything using this photo node.
         if (TPARSE.isTwitterProfilePhoto(node)) {
           processTwitterFollows(node);
         }
@@ -191,53 +185,19 @@ const recordTwitter = function() {
   
   // stackoverflow.com/questions/57468727/when-to-disconnect-mutationobserver-in-chrome-web-extension
   // www.smashingmagazine.com/2019/04/mutationobserver-api-guide/
-  _twitterObserver = new MutationObserver(twitterFollowMutationCallback);
-  _twitterObserver.observe(mainColumn, _mutationSettings);
+  _observer = new MutationObserver(twitterFollowMutationCallback);
+  _observer.observe(mainColumn, _observerSettings);
   processTwitterFollows(mainColumn);
-}
-
-// see comments at Mutation callback
-const buildTwitterFollowFromPhoto = function(img, parsedUrl) {
-  const imgSrc = img.getAttribute('src');
-  const imgAnchor = ES6.findUpTag(img, 'a', false);
-  const profileUrl = imgAnchor.getAttribute('href');
-  const handle = profileUrl.substring(1); // trim the starting '/'
-  const atHandle = TPARSE.twitterHandleFromProfileUrl(profileUrl); 
-  const userCell = TFOLLOW.findUpTwitterUserCell(img);
-  // one is handle, one is description
-  
-  const textAnchors = Array.from(userCell.getElementsByTagName('a')).filter(function(a) { return a != imgAnchor; });
-  
-  const displayNameAnchor = textAnchors.find(function(a) { 
-    return a.innerText && a.innerText.length > 0 && 
-            a.innerText.toLowerCase() != atHandle.toLowerCase(); 
-  });
-  
-  const displayName = ES6.getUnfurledText(displayNameAnchor);
-  const description = getTwitterProfileDescription(displayNameAnchor);
-  
-  // save with @ symbol
-  const per = {
-    handle: atHandle,
-    displayName: displayName,
-    description: description,
-    pageType: parsedUrl.pageType,
-    owner: STR.ensurePrefix(parsedUrl.owner, '@'),
-    imgCdnUrl: imgSrc
-  };
-  
-  per.accounts = STR.extractAccounts([per.displayName, per.description]);
-  return per;
 }
 
 const processTwitterFollows = function(scopeElm) {
   const parsedUrl = URLPARSE.getParsedUrl();
-  const photos = getTwitterFollowImgs(scopeElm);
+  const photos = TPARSE.getTwitterProfilePhotos(scopeElm);
   const ppl = [];
   
   for (let i = 0; i < photos.length; i++) {
     let photo = photos[i];
-    let per = buildTwitterFollowFromPhoto(photo, parsedUrl);
+    let per = TFOLLOW.buildTwitterFollowFromPhoto(photo, parsedUrl);
     ppl.push(per);
   }
   
@@ -245,29 +205,19 @@ const processTwitterFollows = function(scopeElm) {
     for (let i = 0; i < ppl.length; i++) {
       let item = ppl[i];
       let key = `${item.handle}-${item.owner}-${item.pageType}`.toLowerCase();
-      if (!_savableFollowKeySet.has(key) && !_savedFollowHandleSet.has(key)) {
+      if (!_savableKeySet.has(key) && !_savedKeySet.has(key)) {
         // add newly found handles to what we want to save
-        _savableFollows.push(item);
-        _savableFollowKeySet.add(key);
+        _savables.push(item);
+        _savableKeySet.add(key);
         _lastDiscoveryTime = Date.now();
       }
     }
   }
 }
 
-const getTwitterProfileDescription = function(displayNameAnchorElm) {
-  if (!displayNameAnchorElm) { return null; }
-  const parentCell = TFOLLOW.findUpTwitterUserCell(displayNameAnchorElm);
-  if (!parentCell) { return null; }
-  const descripElm = TFOLLOW.findTwitterDescriptionWithinUserCell(parentCell);
-  if (!descripElm) { return null; }
-  let text = ES6.getUnfurledText(descripElm);
-  return text;
-}
-
 const scrollAsNeeded = function(avoidScrollIfHidden) {
   
-  if (_autoScroll != true || !_twitterObserver) {
+  if (_autoScroll != true || !_observer) {
     return;
   }
   
@@ -310,7 +260,7 @@ const scrollAsNeeded = function(avoidScrollIfHidden) {
   
   if (scrollable === true) {
     // did we come up empty between prior run and now?
-    let count = _savableFollowKeySet.size + _savedFollowHandleSet.size;
+    let count = _savableKeySet.size + _savedKeySet.size;
     
     let emptyScroll = count <= _preScrollCount;
     
