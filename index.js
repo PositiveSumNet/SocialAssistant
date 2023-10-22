19c36f52511b1205692b39f2ed05578862d672fb
Vince Scafaria <vince@positivesum.net>
2023-10-22 17:35:40 -0400

    refactored

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ var _deletingMdonRemoteOwner = false;
 var _counterSet = new Set();
 var _counters = [];
 
-// for export
-var _exportStopRequested;
-
 // read out to initialize (using chrome.storage.local is more seure than localStorage)
 chrome.storage.local.get([MASTODON.OAUTH_CACHE_KEY.USER], function(result) {
   _mdonRememberedUser = result.mdonUser || {};
@@ -38,72 +35,8 @@ chrome.storage.local.get([MASTODON.OAUTH_CACHE_KEY.USER_AUTH_TOKEN], function(re
   // console.log('userauth: ' + _mdonUserAuthToken);
 });
 
-const onCopiedToDb = async function(cacheKeys) {
-  // we can clear out the cache keys
-  for (let i = 0; i < cacheKeys.length; i++) {
-    let cacheKey = cacheKeys[i];
-    await chrome.storage.local.remove(cacheKey);
-  }
-  
-  // and queue up the next run
-  await QUERYWORK_UI.ensureCopiedToDb();
-}
 
-// receive messages from _worker
-_worker.onmessage = async function ({ data }) {
-  switch (data.type) {
-    case MSGTYPE.FROMDB.LOG.LEGACY:
-      // legacy + error logging
-      LOG_UI.logHtml(data.payload.cssClass, ...data.payload.args);
-      break;
-    case MSGTYPE.FROMDB.LOG.SQLITE_VERSION:
-      LOG_UI.logSqliteVersion(data.payload);
-      break;
-    case MSGTYPE.FROMDB.LOG.DB_SCRIPT_VERSION:
-      LOG_UI.logDbScriptVersion(data.payload);
-      break;
-    case MSGTYPE.FROMDB.WORKER_READY:
-      TOPICS.ensureRemoteTopicSettings(QUERYWORK_UI.onFetchedRawTopicContent);
-      QUERYWORK_UI.ensureInUseTopicsFilter();
-      await QUERYWORK_UI.ensureCopiedToDb();
-      break;
-    case MSGTYPE.FROMDB.COPIED_TODB:
-      await onCopiedToDb(data.cacheKeys);
-      break;
-    case MSGTYPE.FROMDB.SAVE_AND_DELETE_DONE:
-      QUERYWORK_UI.onCompletedSaveAndDelete(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.SUGGESTED_OWNER:
-      RENDERBIND_UI.renderSuggestedOwner(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.MATCHED_OWNERS:
-      RENDERBIND_UI.renderMatchedOwners(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.CONNECTIONS:
-      RENDERBIND_UI.renderConnections(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.POST_STREAM:
-      RENDERBIND_UI.renderPostStream(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.NETWORK_SIZE:
-      QUERYING_UI.COUNT.renderNetworkSize(data.payload);
-      break;
-    case MSGTYPE.FROMDB.RENDER.INUSE_TOPICS:
-      _inUseTags = new Set(data.payload);
-      QUERYING_UI.FILTERS.TOPICS.adjustTopicFilterVizWhen();
-      break;
-    case MSGTYPE.FROMDB.ON_SUCCESS.SAVED_COUNT:
-      QUERYWORK_UI.onGotSavedCount(data.count, data.pageType, data.metadata);
-      break;
-    case MSGTYPE.FROMDB.ON_FETCHED_FOR_BACKUP:
-      await SYNCFLOW.onFetchedForBackup(data.pushable);
-      break;
-    default:
-      LOG_UI.logHtml('error', 'Unhandled message:', data.type);
-      break;
-  }
-};
-
+QUERYWORK_UI.bindWorker();
 RENDERBIND_UI.bindEvents();
 ES6.TRISTATE.initAll();
 QUERYWORK_UI.bindElements();
