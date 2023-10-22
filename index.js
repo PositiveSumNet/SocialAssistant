71f88aacbeccc3fdee47af811ac0c5cd3401b63f
Vince Scafaria <vince@positivesum.net>
2023-10-22 17:31:59 -0400

    refactoring

--- index.js
+++ index.js
@@ -38,21 +38,6 @@ chrome.storage.local.get([MASTODON.OAUTH_CACHE_KEY.USER_AUTH_TOKEN], function(re
   // console.log('userauth: ' + _mdonUserAuthToken);
 });
 
-// returns back a copy of the saved data
-const onCompletedSaveAndDelete = function(payload) {
-  switch (payload.onSuccessType) {
-    case SETTINGS.REMOTE.LAST_TOPICS_PULL_SUCCESS:
-      localStorage.setItem(SETTINGS.REMOTE.LAST_TOPICS_PULL_SUCCESS, Date.now());
-      break;
-    case MSGTYPE.FROMDB.ON_SUCCESS.SAVED_POST_TAG:
-      TAGGING.POSTS.onTaggingSuccess(payload);
-      break;
-    default:
-      // no-op
-      break;
-  }
-}
-
 const onCopiedToDb = async function(cacheKeys) {
   // we can clear out the cache keys
   for (let i = 0; i < cacheKeys.length; i++) {
@@ -61,183 +46,8 @@ const onCopiedToDb = async function(cacheKeys) {
   }
   
   // and queue up the next run
-  await ensureCopiedToDb();
+  await QUERYWORK_UI.ensureCopiedToDb();
 }
-
-const ensureCopiedToDb = async function() {
-  const kvps = await SETTINGS.getCacheKvps(STORAGE_PREFIX.FOR_DB);
-  
-  const xferring = document.getElementById('transferringMsg');
-  // if concurrent access becomes a problem, we can revert to hiding the list while importing (for now commented out)
-  const filterSet = document.getElementById('listFilterSet');
-  xferring.textContent = 'Copying ' + kvps.length + ' pages to local database...';
-  if (kvps.length > 0) {
-    xferring.style.display = 'inline-block';
-    filterSet.style.display = 'none';
-  }
-  
-  // allow sqlite to do process in larger batches than what was cached
-  const batches = [];
-  const maxBatches = 10;
-  const monoMultiplier = 10; // if these aren't arrays, we'll process 100 items instead of 10 array pages
-  let ctr = 0;
-  let hitArray = false;
-  for (let i = 0; i < kvps.length; i++) {
-    let kvp = kvps[i];
-    batches.push(kvp);
-    ctr++;
-    
-    if (Array.isArray(kvp.val)) {
-      hitArray = true;
-    }
-
-    if ((hitArray == true && ctr >= maxBatches) || (hitArray == false && ctr >= maxBatches * monoMultiplier)) {
-      // come back for more rather than sending massive messages around
-      break;
-    }
-  }
-  
-  if (batches.length > 0) {
-    _worker.postMessage( { batches: batches, actionType: MSGTYPE.TODB.XFER_CACHE_TODB } );
-  }
-  else {
-    // if we got to here, we've fully copied into the db
-    xferring.style.display = 'none';
-    filterSet.style.display = 'flex';
-
-    await initialRender();
-  }
-}
-
-const initialRender = async function(leaveHistoryStackAlone) {
-  // app version
-  document.getElementById('manifestVersion').textContent = chrome.runtime.getManifest().version;
-
-  // ensure _topicTags are in place
-  RENDER.POST.TAGGING.initTopicTags();
-  // now render the combobox dropdown
-  QUERYING_UI.FILTERS.renderTopicFilterChoices();
-  
-  const parms = URLPARSE.getQueryParms();
-
-  let owner = parms[URL_PARM.OWNER];
-  let pageType = parms[URL_PARM.PAGE_TYPE];
-  let topic = parms[URL_PARM.TOPIC];
-  let threadUrlKey = parms[URL_PARM.THREAD];
-
-  if (!pageType) {
-    pageType = SETTINGS.getCachedPageType();
-  }
-  
-  let site;
-  if (pageType) {
-    site = PAGETYPE.getSite(pageType);
-    SETTINGS.cacheSite(site);
-  }
-  else {
-    site = SETTINGS.getCachedSite();
-  }
-
-  QUERYING_UI.PAGE_TYPE.updateUiForCachedSite();
-
-  // pageType/direction
-  let autoResolveOwner = true;
-  pageType = pageType || SETTINGS.getCachedPageType() || PAGETYPE.TWITTER.FOLLOWING;
-
-  switch (pageType) {
-    case PAGETYPE.TWITTER.FOLLOWERS:
-    case PAGETYPE.MASTODON.FOLLOWERS:
-        document.getElementById('cmbType').value = CONN_DIRECTION.FOLLOWERS;
-      break;
-    case PAGETYPE.TWITTER.FOLLOWING:
-    case PAGETYPE.MASTODON.FOLLOWING:
-        document.getElementById('cmbType').value = CONN_DIRECTION.FOLLOWING;
-      break;
-    case PAGETYPE.TWITTER.TWEETS:
-      document.getElementById('cmbType').value = POSTS;
-      autoResolveOwner = false;
-      break;
-    case PAGETYPE.MASTODON.TOOTS:
-      // TEMPORARY - until mastodon toots are ready    
-      document.getElementById('cmbType').value = CONN_DIRECTION.FOLLOWING; // POSTS;
-      autoResolveOwner = false;
-      break;
-    case PAGETYPE.GITHUB.CONFIGURE:
-    case PAGETYPE.GITHUB.BACKUP:
-    case PAGETYPE.GITHUB.RESTORE:
-      autoResolveOwner = false;
-      break;
-    default:
-      break;
-  }
-
-  // set owner
-  let waitForOwnerCallback = false;
-  if (autoResolveOwner) {
-    owner = owner || SETTINGS.getCachedOwner();
-    
-    if (!owner || owner.length === 0) {
-      // we'll initialize to empty string, but 
-      // we'll tell the _worker to call us back with the most sensible initial value
-      const msg = { 
-        actionType: MSGTYPE.TODB.SUGGEST_OWNER, 
-        pageType: pageType
-      };
-      
-      waitForOwnerCallback = true;
-      _worker.postMessage(msg);
-    }
-  }
-
-  // SEARCH
-  txtSearch.value = parms[URL_PARM.SEARCH] || '';
-
-  // PAGING
-  let page = parms[URL_PARM.PAGE];
-  if (!page || isNaN(page) == true) {
-    page = 1;
-  }
-  txtPageNum.value = page;
-
-  // post toggles
-  // WITH_RETWEETS
-  const optWithRetweets = document.getElementById('optWithRetweets');
-  QUERYING_UI.FILTERS.setOptToggleBtn(optWithRetweets, parms[URL_PARM.WITH_RETWEETS] != 'false'); // default to true
-
-  // TOPIC
-  const optGuessTopics = document.getElementById('optGuessTopics');
-  QUERYING_UI.FILTERS.setOptToggleBtn(optGuessTopics, parms[URL_PARM.GUESS_TOPICS] == 'true'); // default to false
-  QUERYING_UI.FILTERS.TOPICS.setTopicFilterVisibility();
-  QUERYING_UI.FILTERS.TOPICS.setTopicFilterChoiceInUi(topic);
-  // THREAD
-  QUERYING_UI.THREAD.setOneThreadState(threadUrlKey);
-  // post sort
-  QUERYING_UI.ORDERING.setTopicSortInUi();
-
-  QUERYING_UI.FILTERS.setQueryOptionVisibility();
-
-  txtOwnerHandle.value = STR.stripPrefix(owner, '@') || '';
-  
-  if (waitForOwnerCallback === false) {
-    
-    switch (site) {
-      case SITE.GITHUB:
-        await TABS_UI.SYNC.activateGithubTab(pageType);
-        _docLocSearch = document.location.search; // aids our popstate behavior
-        break;
-      default:
-        QUERYWORK_UI.executeSearch(owner, leaveHistoryStackAlone, topic);
-        break;
-    }
-  }
-}
-
-// companion to the above pushState so that back button works
-window.addEventListener("popstate", async function(event) {
-  if (_docLocSearch != document.location.search) {
-    await initialRender(true);
-  }
-});
 
 // receive messages from _worker
 _worker.onmessage = async function ({ data }) {
@@ -255,13 +65,13 @@ _worker.onmessage = async function ({ data }) {
     case MSGTYPE.FROMDB.WORKER_READY:
       TOPICS.ensureRemoteTopicSettings(QUERYWORK_UI.onFetchedRawTopicContent);
       QUERYWORK_UI.ensureInUseTopicsFilter();
-      await ensureCopiedToDb();
+      await QUERYWORK_UI.ensureCopiedToDb();
       break;
     case MSGTYPE.FROMDB.COPIED_TODB:
       await onCopiedToDb(data.cacheKeys);
       break;
     case MSGTYPE.FROMDB.SAVE_AND_DELETE_DONE:
-      onCompletedSaveAndDelete(data.payload);
+      QUERYWORK_UI.onCompletedSaveAndDelete(data.payload);
       break;
     case MSGTYPE.FROMDB.RENDER.SUGGESTED_OWNER:
       RENDERBIND_UI.renderSuggestedOwner(data.payload);
@@ -294,6 +104,7 @@ _worker.onmessage = async function ({ data }) {
   }
 };
 
+RENDERBIND_UI.bindEvents();
 ES6.TRISTATE.initAll();
 QUERYWORK_UI.bindElements();
 SETTINGS_UI.bindElements();
