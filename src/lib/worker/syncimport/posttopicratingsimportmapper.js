c7917ae262fefff2a2f73ba57619462175b10aae
Vince Scafaria <vince@positivesum.net>
2023-11-10 09:17:45 -0500

    avoid getting stuck on topics

--- src/lib/worker/syncimport/posttopicratingsimportmapper.js
+++ src/lib/worker/syncimport/posttopicratingsimportmapper.js
@@ -9,13 +9,22 @@ var POST_TOPIC_RATINGS_IMPORT_MAPPER = {
     
     // RDF - subject, object, graph, timestamp
     const ratings = records.map(function(x) {
-      return {
-        s: x[SYNC_COL.RATED_POST.PostUrlKey], 
-        o: x[SYNC_COL.RATED_POST.Concat], 
-        g: x[SCHEMA_CONSTANTS.COLUMNS.NamedGraph] || APPGRAPHS.MYSELF,
-        t: x[SCHEMA_CONSTANTS.COLUMNS.Timestamp]
-      };
-    });
+      let topic = x[SYNC_COL.RATED_POST.Topic];
+      let subtopic = x[SYNC_COL.RATED_POST.Subtopic];
+      if (STR.hasLen(topic) && STR.hasLen(subtopic)) {
+        let concat = TOPICS.concatTopicFullName(topic, subtopic);
+
+        return {
+          s: x[SYNC_COL.RATED_POST.PostUrlKey], 
+          o: concat, 
+          g: x[SCHEMA_CONSTANTS.COLUMNS.NamedGraph] || APPGRAPHS.MYSELF,
+          t: x[SCHEMA_CONSTANTS.COLUMNS.Timestamp]
+        };
+      }
+      else {
+        return null;
+      }
+    }).filter(function(r) { return r != null; });
 
     APPSCHEMA.SAVING.getSubset(set, entTopicRating.Name).sogs = ratings;
 
